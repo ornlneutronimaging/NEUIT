ccea1b9a61f09c2a0242d217f2264b63b48e2663
Yuxuan Zhang <zhangy6x@gmail.com>
2018-02-20 18:07:22 -0500

    added energy slider

--- app.py
+++ app.py
@@ -26,97 +26,6 @@ import pprint
 app = dash.Dash(__name__)
 # app_flask = app.server
 
-# app.layout = html.Div(children=[
-#     html.H1(children='Hello Dash'),
-#     html.Div([
-#         html.Div(
-#             dcc.Input(id='input_formula',
-#                       name='formula',
-#                       placeholder='Enter chemical formula...',
-#                       type='text',
-#                       value='',
-#                       ),
-#             # dcc.Input(id='input_thickness',
-#             #           name='thickness',
-#             #           placeholder='Enter thickness (mm)...',
-#             #           type='number',
-#             #           value='',
-#             #           ),
-#             # dcc.Input(id='input_density',
-#             #           name='density',
-#             #           placeholder='Enter density (g/cm3)...',
-#             #           type='number',
-#             #           value='',
-#             #           ),
-#         ),
-#         html.Button('Submit', id='button'),
-#         html.Div(id='output-container-button',
-#                  children='Enter a value and press submit')
-#     ])
-#     ,
-#     html.Hr(),
-#     html.Div(children='''
-#             Dash: A web application framework for Python.
-#         '''),
-#
-#     dcc.Graph(
-#         id='example-graph',
-#         figure={
-#             'data': [
-#                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-#                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-#             ],
-#             'layout': {
-#                 'title': 'Dash Data Visualization'
-#             }
-#         }
-#     )
-# ])
-# @app.callback(
-#     dash.dependencies.Output('output-container-button', 'children'),
-#     [dash.dependencies.Input('button', 'n_clicks')],
-#     [dash.dependencies.State('input_formula', 'value')])
-# def update_output(n_clicks, value):
-#     return 'The input value was "{}" and the button has been clicked {} times'.format(
-#         value,
-#         n_clicks
-#     )
-
-
-# app.layout = html.Div([
-#     # represents the URL bar, doesn't render anything
-#     dcc.Location(id='url', refresh=False),
-#
-#     dcc.Link('Navigate to "/"', href='/'),
-#     html.Br(),
-#     dcc.Link('Navigate to "/page-2"', href='/page-2'),
-#     # content will be rendered in this element
-#     html.Div(id='page-content'),
-#
-#     html.Div(
-#         dcc.Input(id='input_formula',
-#                   name='formula',
-#                   placeholder='Enter chemical formula...',
-#                   type='text',
-#                   value='',
-#                   ),
-#     ),
-#     html.Button('Submit', id='button'),
-#     html.Div(id='output-container-button',
-#              children='Enter a value and press submit')
-# ])
-#
-#
-# @app.callback(
-#     dash.dependencies.Output('output-container-button', 'children'),
-#     [dash.dependencies.Input('button', 'n_clicks')],
-#     [dash.dependencies.State('input_formula', 'value')])
-# def update_output(n_clicks, value):
-#     return 'The input value was "{}" and the button has been clicked {} times'.format(
-#         value,
-#         n_clicks
-#     )
-
 
 app.layout = html.Div([
     html.Button('Click Me', id='button'),
@@ -125,64 +34,158 @@ app.layout = html.Div([
     html.Hr(),
 
     html.Label('Energy min. (eV)'),
-    dcc.Input(id='e_min', value=1),
+    dcc.Input(id='e_min', value=1, type='number', min=0.000001),
 
     html.Label('Energy max. (eV)'),
-    dcc.Input(id='e_max', value=300),
+    dcc.Input(id='e_max', value=300, type='number', min='e_min', max=10000000),
 
     html.Label('Energy step (eV)'),
-    dcc.Input(id='e_step', value=0.1),
+    dcc.Input(id='e_step', value=0.1, type='number', min=0.0001),
 
     html.Hr(),
 
     html.Label('Chemical formula'),
-    dcc.Input(id='formula', value='Ag'),
+    dcc.Input(id='formula', value='Ag', type='text', minlength=1),
 
     html.Label('Thickness (mm)'),
-    dcc.Input(id='thickness', value=0.5),
+    dcc.Input(id='thickness', value=0.5, type='number', min=0),
 
-    html.Label('Density (g/cm3)'),
-    dcc.Input(id='density', value=2),
+    html.Label('Density (g/cm3) *Input is optional for solid single element layer'),
+    dcc.Input(id='density', value=None, type='number', min=0, placeholder='Optional'),
 
     # html.Label('Slider 1'),
     # dcc.Slider(id='slider-1'),
     html.Br(),
 
-    html.Button('Show stack', id='button-2'),
+    html.Button('Submit', id='button-2'),
+    html.Hr(),
 
-    html.Div(id='output')
+    html.Div(id='stack'),
+    html.Hr(),
+
+    # html.Button('Show plot', id='button-3'),
+    # html.Hr(),
+    html.Div([
+        dcc.Slider(
+            id='e_step_slider',
+            min=0.0001,
+            max=1,
+            value=0.01,
+            dots=False,
+            step=0.0001,
+            updatemode='drag',
+            marks={i: '{}'.format(10 ** i) for i in range(5)},
+        ),
+        html.Div(id='e_step_slider_container', style={'margin-top': 20}),
+
+        #
+        dcc.RangeSlider(id='e_range_slider',
+                        min=0,
+                        max=5,
+                        value=[1, 2],
+                        allowCross=False,
+                        dots=False,
+                        step=0.01,
+                        marks={i: '{}'.format(10 ** i) for i in range(5)},
+                        updatemode='drag'
+                        ),
+        html.Div(id='e_range_slider_container', style={'margin-top': 20}),
+    ]),
+
+    dcc.Graph(id='plot'),
 ])
+
+
+def transform_value(value):
+    return 10 ** value
+
+
+@app.callback(
+    Output('e_step_slider_container', 'children'),
+    [Input('e_step_slider', 'value')])
+def update_output(value):
+    transformed_value = transform_value(value)
+    return 'Linear Value: {}, Log Value: [{:0.2f}]'.format(
+        str(value),
+        transformed_value,
+        # transformed_value[
+    )
+
+
+@app.callback(
+    Output('e_range_slider_container', 'children'),
+    [Input('e_range_slider', 'value')])
+def update_output(value):
+    transformed_value = [transform_value(v) for v in value]
+    return 'Linear Value: {}, Log Value: [{:0.2f}, {:0.2f}]'.format(
+        str(value),
+        transformed_value[0],
+        transformed_value[1]
+    )
 
 
 @app.callback(
     Output('button-clicks', 'children'),
-    [Input('button', 'n_clicks')])
+    [Input('button', 'n_clicks')],
+)
 def clicks(n_clicks):
     return 'Button has been clicked {} times'.format(n_clicks)
 
 
 @app.callback(
-    Output('output', 'children'),
-    [Input('button-2', 'n_clicks')],
-    state=[State('e_min', 'value'),
-           State('e_max', 'value'),
-           State('e_step', 'value'),
-           State('formula', 'value'),
-           State('thickness', 'value'),
-           State('density', 'value')])
+    Output('stack', 'children'),
+    [Input('button-2', 'n_clicks'),
+     Input('e_min', 'value'),
+     Input('e_max', 'value'),
+     Input('e_step', 'value'),
+     Input('formula', 'value'),
+     Input('thickness', 'value'),
+     Input('density', 'value')])
 def compute(n_clicks, e_min, e_max, e_step, formula, thickness, density):
-    o_reso = Resonance(energy_min=e_min, energy_max=e_max, energy_step=e_step)
+    if n_clicks is not None:
+        o_reso = Resonance(energy_min=e_min, energy_max=e_max, energy_step=e_step)
+        if density is not None:
+            o_reso.add_layer(formula=formula,
+                             thickness=thickness,
+                             density=density)
+        else:
+            o_reso.add_layer(formula=formula,
+                             thickness=thickness)
+        stack = o_reso.stack
+        p_stack = pprint.pformat(o_reso.stack)
+        layer = list(stack.keys())
+        for each_layer in stack.keys():
+            current_layer = stack[each_layer]
+            elements = current_layer['elements']
+        return '{}\nStack: {}\nLayer: {}\nelement: {}\n'.format(n_clicks, p_stack, layer, elements)
+    else:
+        return None
 
-    o_reso.add_layer(formula,
-                     thickness,
-                     density)
-    stack = o_reso.stack
-    p_stack = pprint.pformat(o_reso.stack)
-    layer = list(stack.keys())
-    for each_layer in stack.keys():
-        current_layer = stack[each_layer]
-        elements = current_layer['elements']
-    return 'Stack: {}\nLayer: {}\nelement: {}\n'.format(p_stack, layer, elements)
+
+@app.callback(
+    Output('plot', 'figure'),
+    [Input('button-2', 'n_clicks'),
+     Input('e_min', 'value'),
+     Input('e_max', 'value'),
+     Input('e_step', 'value'),
+     Input('formula', 'value'),
+     Input('thickness', 'value'),
+     Input('density', 'value')])
+def plot(n_clicks, e_min, e_max, e_step, formula, thickness, density):
+    if n_clicks is not None:
+        o_reso = Resonance(energy_min=e_min, energy_max=e_max, energy_step=e_step)
+        if density is not None:
+            o_reso.add_layer(formula=formula,
+                             thickness=thickness,
+                             density=density)
+        else:
+            o_reso.add_layer(formula=formula,
+                             thickness=thickness)
+        plotly_fig = o_reso.plot(plotly=True, all_elements=True, all_isotopes=True)
+        plotly_fig.layout.showlegend = True
+        return plotly_fig
+    else:
+        return None
 
 
 # @app.callback(dash.dependencies.Output('page-content', 'children'),
