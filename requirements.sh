2d966db8cd7b79ac2da98dd0f23dc8d673384eff
Yuxuan Zhang <zhangy6x@gmail.com>
2018-03-29 12:20:06 -0400

    updated requirements

--- requirements.sh
+++ requirements.sh
@@ -5,3 +5,7 @@ pip install dash-html-components==0.9.0  # HTML components
 pip install dash-core-components==0.21.0  # Supercharged components
 pip install dash-table-experiments  # Interactive table components
 pip install plotly
+pip install ImagingReso==1.6.3
+pip install pandas
+pip install numpy
+pip install scipy
