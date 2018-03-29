532978d64647bcf6269e2eb96262de7e56905651
Yuxuan Zhang <zhangy6x@gmail.com>
2018-03-29 12:57:07 -0400

    updated requirements

--- requirements.sh
+++ requirements.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 pip install dash  # The core dash backend
-pip install dash-renderer==0.11.3  # The dash front-end
-pip install dash-html-components==0.9.0  # HTML components
-pip install dash-core-components==0.21.0  # Supercharged components
+pip install dash-renderer  # The dash front-end
+pip install dash-html-components  # HTML components
+pip install dash-core-components  # Supercharged components
 pip install dash-table-experiments  # Interactive table components
 pip install plotly
-pip install ImagingReso==1.6.3
+pip install ImagingReso
 pip install pandas
 pip install numpy
 pip install scipy
