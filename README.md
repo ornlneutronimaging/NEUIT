535a6d06bc81c5db38412193d8a7837971115eba
Yuxuan Zhang <zhangy6x@gmail.com>
2018-04-03 16:11:11 -0400

    added matplotlib for deployment

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # NeuWeb
 
-A prototype of a web sample estimator for neutron imaging
+A prototype of a web sample estimator for neutron imaging.
--- apps/app2.py
+++ apps/app2.py
@@ -6,7 +6,6 @@ import pandas as pd
 from ImagingReso._utilities import ev_to_angstroms
 from ImagingReso._utilities import ev_to_s
 from dash.dependencies import Input, Output, State
-import pprint
 from _app import app
 from _utilities import init_reso_from_tb, unpack_sample_tb_df_and_add_layer, \
     add_del_tb_rows, plot_option_div, \
