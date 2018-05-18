e38618c27ccfa2a05dba414274d4edaaf336a926
Yuxuan Zhang <zhangy6x@gmail.com>
2018-05-18 17:50:41 -0400

    removed unused code

--- apps/app2.py
+++ apps/app2.py
@@ -1,6 +1,4 @@
 import numpy as np
-import flask
-import io
 from ImagingReso._utilities import ev_to_angstroms
 from ImagingReso._utilities import ev_to_s
 from dash.dependencies import Input, Output, State
@@ -397,28 +395,6 @@ def plot(n_submit, y_type, x_type, plot_scale, show_opt,
                 dcc.Graph(id='reso_plot', figure=plotly_fig, className='container'),
             ]
         )
-
-
-@app.callback(Output('my-link', 'href'), [Input('e_step', 'value')])
-def update_link(value):
-    return '/dash/urlToDownload?value={}'.format(value)
-
-
-@app.server.route('/dash/urlToDownload')
-def download_csv():
-    value = flask.request.args.get('value')
-    # create a dynamic csv or file here using `StringIO`
-    # (instead of writing to the file system)
-    str_io = io.StringIO()
-    str_io.write('You have selected {}'.format(value))
-    mem = io.BytesIO()
-    mem.write(str_io.getvalue().encode('utf-8'))
-    mem.seek(0)
-    str_io.close()
-    return flask.send_file(mem,
-                           mimetype='text/csv',
-                           attachment_filename='downloadFile.csv',
-                           as_attachment=True)
 
 
 @app.callback(
