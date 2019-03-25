58bdeb5ac47af6a4002e54567ee6d8bd16e77447
YuxuanZhang <zhangy6x@gmail.com>
2019-03-25 00:46:17 -0400

    Use latest ImagingReso

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pip install dash>=0.39.0  # The core dash backend
 pip install plotly>=3.7.1
-pip install ImagingReso >= 1.6.15
+pip install ImagingReso >= 1.6.16
 pip install matplotlib
 pip install pandas
 pip install numpy
