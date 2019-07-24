41137690357bb9b180faabcf50faf0274f75574c
YuxuanZhang <zhangy6x@gmail.com>
2019-07-23 22:10:04 -0400

    limit the dash version due to compatibility

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-pip install dash>=0.39.0  # The core dash backend
-pip install plotly>=3.7.1
-pip install ImagingReso >= 1.6.16
+pip install dash==0.41.0 # The core dash backend
+pip install dash-core-components==0.46.0
+pip install dash-html-components==0.15.0
+pip install dash-renderer==0.22.0
+pip install dash-table==3.6.0
+pip install plotly==3.10.0
+pip install ImagingReso>=1.6.16
 pip install matplotlib
 pip install pandas
 pip install numpy
