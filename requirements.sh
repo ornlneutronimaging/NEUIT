b11f6e341d653e726383bc15291c7cd8ffb087fa
YuxuanZhang <zhangy6x@gmail.com>
2019-08-21 23:45:20 -0400

    bump plotly to 4.1.0

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pip install dash>=1.1.1  # The core dash backend
 pip install dash-table>=4.1.0
-pip install plotly>=3.10.0
+pip install plotly>=4.1.0
 pip install ImagingReso>=1.6.16
 pip install matplotlib>=3.1.1
 pip install pandas>=0.25.0
