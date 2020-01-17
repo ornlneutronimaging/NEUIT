f92dd181d9401e1ef807b7e4872eb0077140a9bd
Yuxuan Zhang <zhangy6x@gmail.com>
2020-01-17 09:24:45 -0500

    - Updated index.py

--- index.py
+++ index.py
@@ -65,14 +65,12 @@ a DOE Office of Science User Facility operated by Oak Ridge National Laboratory.
         dcc.Markdown('''
 #### Cite our work
 
-1. Yuxuan Zhang, Jean Bilheux, Hassina Bilheux and Jiao Lin, (2019) "An interactive web-based tool 
-to guide the preparation of neutron imaging experiments at oak ridge national laboratory", 
-Journal of Physics Communications, 3(10), 103003,
-[doi:10.1088/2399-6528/ab4ee6](https://iopscience.iop.org/article/10.1088/2399-6528/ab4ee6).
+1.Yuxuan Zhang, Jean Bilheux, Hassina Bilheux and Jiao Lin, (2019) "[An interactive web-based tool 
+to guide the preparation of neutron imaging experiments at oak ridge national laboratory](https://iopscience.iop.org/article/10.1088/2399-6528/ab4ee6)", 
+*Journal of Physics Communications*, 3(10), 103003.
 
-2. Yuxuan Zhang and Jean Bilheux, (2017), "ImagingReso: A Tool for Neutron Resonance Imaging",
-Journal of Open Source Software, 2(19), 407,
-[doi:10.21105/joss.00407](http://joss.theoj.org/papers/997d09281a9d76e95f4ec4d3279eeb8c).
+2.Yuxuan Zhang and Jean Bilheux, (2017), "[ImagingReso: A Tool for Neutron Resonance Imaging](http://joss.theoj.org/papers/997d09281a9d76e95f4ec4d3279eeb8c)",
+*Journal of Open Source Software*, 2(19), 407.
 
 '''),
         dcc.Markdown('''
