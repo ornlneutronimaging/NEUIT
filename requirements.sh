377e4892184cc89ffae3acd758bd86d64dc29198
Yuxuan Zhang <zhangy6x@gmail.com>
2021-01-20 11:34:36 -0500

    fixing dependencies version issue

--- requirements.sh
+++ requirements.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 pip install dash==1.18.1  # The core dash backend
 pip install dash-table==4.11.1
-pip install plotly==4.14.3
-pip install ImagingReso==1.7.3
+pip install plotly>=4.14.1
+pip install ImagingReso>=1.7.3
 pip install matplotlib==3.1.3
-pip install pandas==1.2.0
+pip install pandas>=1.1.3
 pip install numpy==1.19.2
-pip install scipy==1.6.0
+pip install scipy>=1.5.2
 pip install cerberus==1.3.2
 pip install gunicorn==20.0.4
