efbcbec2b870d4b7dcf2d8b2980f28ace4acd791
JeanBilheux <bilheuxjm@ornl.gov>
2021-12-07 09:16:57 -0500

    golden angles. this refs #11

---
+++ callbacks/golden_angles.py
@@ -0,0 +1,49 @@
+from dash.dependencies import Output, Input
+import plotly.graph_objects as go
+from app import app
+
+from _utilities import *
+
+app_name = 'app6'
+app_id_dict = init_app_ids(app_name=app_name)
+
+
+def calculate_golden_ratio(angle_index, max_angle=180):
+    phi = 0.5 * (1 + np.sqrt(5))
+    return np.fmod(int(angle_index) * phi * 180, int(max_angle))
+
+
+def retrieve_list_of_golden_angles(total_number_of_angles, max_angle=180):
+    list_angles = [calculate_golden_ratio(angle_index, max_angle=max_angle) for angle_index in np.arange(
+            total_number_of_angles)]
+    return list_angles
+
+
+@app.callback(Output('plot', 'figure'),
+              Output('table', 'children'),
+              Input('max_angle', 'value'),
+              Input('number_of_angles', 'value'))
+def update_golden_angles(max_angle_value, total_number_of_angles):
+    fig = go.Figure()
+
+    list_angles = retrieve_list_of_golden_angles(total_number_of_angles, max_angle=max_angle_value)
+    data = pd.DataFrame(list_angles, columns=['angles'])
+
+    fig.add_trace(go.Scatterpolar(theta=data['angles'],
+                                  mode='markers+lines'))
+    fig.layout.xaxis.title = "Iteration number"
+    fig.layout.paper_bgcolor = '#E5ECF6'
+
+    table = dt.DataTable(columns=([{'id': 'angles',
+                                    'name': 'Angles (degrees)',
+                                    'type': 'numeric',
+                                    'editable': False}]),
+                         data=data.to_dict('records'),
+                         export_format='csv',
+                         style_header={'backgroundColor': 'rgb(30, 30, 30)',
+                                       'color': 'white'},
+                         style_cell={'textAlign': 'left'},
+                         style_table={'maxWidth': '30%'},
+                         )
+
+    return fig, table
