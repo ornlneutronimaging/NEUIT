24e5eb428e4e4ca598c27ad9cdc369a2dd3999f0
JeanBilheux <bilheuxjm@ornl.gov>
2023-01-12 13:37:43 -0500

    Replaced most of the pip to conda. this refs #16

--- requirements.sh
+++ requirements.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-pip install dash==2.1.0  # The core dash backend
-pip install dash-table==5.0.0
-pip install plotly==5.6.0
-pip install ImagingReso>=1.7.4
-conda install braggedgemodeling
-conda install diffpy.structure
-pip install matplotlib==3.1.3
-pip install pandas==1.3.5
-pip install numpy==1.21.5
-pip install scipy==1.7.3
-pip install cerberus==1.3.4
-pip install gunicorn==20.1.0
-pip install dash-bootstrap-components
+conda -y install dash
+conda -y install dash-table
+pip install ImagingReso
+conda -y install braggedgemodeling
+conda -y install matplotlib
+conda -y install numpy
+conda -y install pandas
+conda -y install cerberus
+conda -y install unicorn
+conda -y install dash-bootstrap-components
